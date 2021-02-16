e17b851bcce62c28716b2f5da2aa269ea4ed211e
Pawel <kwiatkowskip@wp.pl>
2021-02-16 11:46:12 +0100

    interface changes

--- sandbox/new_interface/main.py
+++ sandbox/new_interface/main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+'''@app.get("/")
 def index():
-    return{"title": "Hello Coder"}
+    return{"title": "Hello Coder"}'''
+
+db=[]
+
+class User(BaseModel):
+    imie: str
+    nazwisko: str
+    email: str
+    bank : str
+    wydatek: str
+    kwota: int
+
+@app.get('/')
+def index():
+    return {'key' : 'value'}
+    
+@app.get('/users')
+def get_users():
+    
+    return db
+
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
+    return db[user_id-1]
+@app.post('/users')
+def create_user(user: User):
+    db.append(user.dict())
+    return db[-1]
+@app.delete('/user/{user_id}')
+def delete_user(user_id: int):
+    db.pop(user_id-1)
+    return{}
